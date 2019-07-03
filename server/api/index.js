602f9727ecca036308390194cdb6d6013243fed1
Wei Ji <monokuroai@gmail.com>
2019-07-03 18:36:31 -0400

    refactored some screens and attempting to add upload functionality to drawing component

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/tags', require('./location'))
+router.use('/tags', require('./tag'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
