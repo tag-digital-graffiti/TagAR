ce8b3d3cb8bad1fd707363545a9b6061cadfcd94
Madeline Emde <madeline.emde@gmail.com>
2019-07-05 15:00:53 -0400

    updated navigation to navigate from nearby tags to selected before entering AR

--- server/api/tag.js
+++ server/api/tag.js
@@ -1,26 +1,26 @@
-const router = require('express').Router()
-const { Tag } = require('../db/models')
-const sequelize = require('sequelize')
-const cloudinary = require('cloudinary').v2
+const router = require('express').Router();
+const { Tag } = require('../db/models');
+const sequelize = require('sequelize');
+const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
   cloud_name: 'coolcaps',
   api_key: '851696833748766',
   api_secret: '6xc3M9VoKgFxcLO2apfGdu6e0xs'
-})
+});
 
-const Op = sequelize.Op
+const Op = sequelize.Op;
 router.get('/', async (req, res, next) => {
-  const lat = parseFloat(req.query.lat)
-  const long = parseFloat(req.query.long)
+  const lat = parseFloat(req.query.lat);
+  const long = parseFloat(req.query.long);
   try {
     const getNearByTag = await Tag.findAll({
       where: {
         lat: {
-          [Op.between]: [lat - 0.002, lat + 0.002]
+          [Op.between]: [lat - 0.005, lat + 0.005]
         },
         long: {
-          [Op.between]: [long - 0.002, long + 0.002]
+          [Op.between]: [long - 0.005, long + 0.005]
         }
       }
     });
@@ -52,18 +52,20 @@ router.post('/', async (req, res, next) => {
     let lat = req.body.lat;
     let long = req.body.long;
     let imageData = req.body.imageData;
-    await cloudinary.uploader.upload(`data:image/png;base64,${imageData}`, async function (error, result) {
-      if (result) {
-        const arTagUrl = result.url
-        try {
-          await Tag.create({ lat, long, arTagUrl })
-        } catch (error) {
-          next(error)
+    await cloudinary.uploader.upload(
+      `data:image/png;base64,${imageData}`,
+      async function(error, result) {
+        if (result) {
+          const arTagUrl = result.url;
+          try {
+            await Tag.create({ lat, long, arTagUrl });
+          } catch (error) {
+            next(error);
+          }
         }
       }
-    })
+    );
     res.end();
-
   } catch (error) {
     next(error);
   }
