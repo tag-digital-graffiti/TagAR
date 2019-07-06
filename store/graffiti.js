44196b9d01e9731949d245dbf35930a8d4717147
Talia Fayaz <taliafayazc@gmail.com>
2019-07-06 19:21:08 -0400

    action creator thunk and reducer wired up

--- store/graffiti.js
+++ store/graffiti.js
@@ -1,18 +1,25 @@
 import axios from 'axios';
 
 // const server = 'http://tag-sever-ar.herokuapp.com';
-const server = 'http://172.16.26.218:8080';
+const server = 'http://192.168.1.155:8080';
 
 const GET_NEARBY_TAGS = 'GET_NEARBY_TAG';
+const GET_ALL_TAGS = 'GET_ALL_TAGS';
 const GET_SELECTED_TAG = 'GET_SELECTED_TAG';
 
 const initialState = {
   nearByTags: [],
-  selectedTag: {}
+  selectedTag: {},
+  allTags: []
 };
 
 const gotNearbyTags = tags => ({
   type: GET_NEARBY_TAGS,
+  tags
+});
+
+const gotAllTags = tags => ({
+  type: GET_ALL_TAGS,
   tags
 });
 
@@ -34,6 +41,17 @@ export const getNearbyTags = (lat, long) => {
   };
 };
 
+export const getAllTags = () => {
+  return async dispatch => {
+    try {
+      let { data } = await axios.get(`${server}/api/tags/tags`);
+      dispatch(gotAllTags(data));
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+};
+
 export const getSelectedTag = id => {
   return async dispatch => {
     try {
@@ -49,6 +67,8 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_NEARBY_TAGS:
       return { ...state, nearByTags: action.tags };
+    case GET_ALL_TAGS:
+      return { ...state, allTags: action.tags };
     case GET_SELECTED_TAG:
       return { ...state, selectedTag: action.tag };
     default:
