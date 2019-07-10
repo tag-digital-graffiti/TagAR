d247068ee665aee3152028c9c2608a3c034d1e7f
Talia Fayaz <taliafayazc@gmail.com>
2019-07-10 15:03:57 -0400

    styled user profile

--- components/Profile.js
+++ components/Profile.js
@@ -1,134 +1,152 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image, Button, AsyncStorage } from 'react-native';
-import { connect } from 'react-redux'
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+  AsyncStorage
+} from 'react-native';
+import { connect } from 'react-redux';
 import { Card } from 'react-native-elements';
-import { FlatGrid } from "react-native-super-grid"
-import { getTagsByArtist } from '../store/graffiti'
+import { FlatGrid } from 'react-native-super-grid';
+import { getTagsByArtist } from '../store/graffiti';
 
 const styles = StyleSheet.create({
   profileView: {
-    backgroundColor: "rgb(244, 242, 244)",
-    flex: 1,
+    backgroundColor: '#262525',
+    flex: 1
   },
   headerView: {
-    backgroundColor: "white",
+    backgroundColor: '#262525',
     height: 250,
     marginTop: 60,
-    alignItems: "center",
+    alignItems: 'center'
   },
   avatarTempImage: {
-    backgroundColor: "transparent",
-    resizeMode: "center",
+    backgroundColor: 'transparent',
+    resizeMode: 'center',
     width: 124,
     height: 124,
-    marginTop: 25,
+    marginTop: 25
   },
   drWhatText: {
-    color: "rgb(4, 12, 22)",
+    color: '#A89898',
     fontSize: 26,
-    fontStyle: "normal",
-    fontWeight: "normal",
-    textAlign: "center",
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    textAlign: 'center',
     lineHeight: 32,
-    backgroundColor: "transparent",
-    marginTop: 11,
+    backgroundColor: 'transparent',
+    marginTop: 11
   },
   galleryView: {
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
     flex: 1,
     marginLeft: 2,
     marginRight: 2,
     marginTop: 20,
     marginBottom: 52,
-    alignItems: "center",
+    alignItems: 'center'
   },
   latestPhotosText: {
-    color: "black",
-    fontFamily: ".AppleSystemUIFont",
-    fontSize: 12,
-    fontStyle: "normal",
-    fontWeight: "normal",
-    textAlign: "left",
-    backgroundColor: "transparent",
-    opacity: 0.4,
+    color: '#FFFFFF',
+    fontFamily: '.AppleSystemUIFont',
+    fontSize: 13,
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    textAlign: 'left',
+    // backgroundColor: 'transparent',
+    opacity: 0.7
   },
   photosFlatList: {
-    backgroundColor: "transparent",
-    width: "100%",
-    height: "100%",
+    // backgroundColor: 'transparent',
+    width: '100%',
+    height: '100%'
   },
   photosFlatListViewWrapper: {
-    alignSelf: "stretch",
+    alignSelf: 'stretch',
     height: 180,
-    marginTop: 13,
+    marginTop: 13
   },
   photosView: {
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
     width: 180,
     height: 180,
-    justifyContent: "center",
-    alignItems: "flex-start",
+    justifyContent: 'center',
+    alignItems: 'flex-start'
   },
   photosImage: {
-    backgroundColor: "transparent",
-    resizeMode: "center",
+    backgroundColor: 'transparent',
+    resizeMode: 'center',
     width: 180,
-    height: 180,
-  },
-})
-
+    height: 180
+  }
+});
 
 class Profile extends Component {
+  _logout = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate('Auth');
+  };
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerStyle: {
+        backgroundColor: '#262525'
+      },
+      headerTintColor: '#A89898',
+      headerRight: (
+        <Button
+          onPress={navigation.getParam('logout')}
+          title="Logout"
+          color="#A89898"
+          fontWeight="bold"
+        />
+      )
+    };
+  };
+
   async componentDidMount() {
-    await this.props.getTagsByArtist(this.props.user.id)
+    this.props.navigation.setParams({ logout: this._logout });
+    await this.props.getTagsByArtist(this.props.user.id);
   }
 
-  _logout = async () => {
-    await AsyncStorage.clear()
-    this.props.navigation.navigate('Auth');
-  }
   render() {
     const user = this.props.user;
     const latestWorks = this.props.artistTags.reverse();
     return (
-      <View
-        style={styles.profileView}>
-        <View
-          style={styles.headerView}>
+      <View style={styles.profileView}>
+        <View style={styles.headerView}>
           <Image
-            source={{ uri: 'https://external-preview.redd.it/3mP_m1XCb2PEpXadWhFURkalTEWwjVO6M_jn5d_pNQ0.jpg?auto=webp&s=b4e62db7ee80fdcd860bef95154e6b738864222d' }}
-            style={styles.avatarTempImage} />
-          <Text
-            style={styles.drWhatText}>{user.username}</Text>
-
-          <View
-            style={{
-              flex: 1,
-            }} />
-          <Button style={styles.drWhatText} title="Logout" onPress={this._logout}></Button>
-
+            source={require('../public/user-placeholder.png')}
+            style={styles.avatarTempImage}
+          />
+          <Text style={styles.drWhatText}>{user.username}</Text>
         </View>
 
-        <View
-          style={styles.galleryView}>
-          <Text
-            style={styles.latestPhotosText}>LATEST CONTENT</Text>
-          <View
-            style={styles.photosFlatListViewWrapper}>
-            {this.props.artistTags[0] ? (<FlatGrid
-              items={latestWorks}
-              renderItem={({ item }) => (
-                <View
-                  style={styles.photosView}>
-                  <Image
-                    source={{ uri: item.arTagUrl }}
-                    style={styles.photosImage} />
-                </View>)}
-              itemDimension={180}
-              horizontal={true}
-              spacing={10}
-              style={styles.photosFlatList} />) :
-              (<Text style={styles.drWhatText}>No User Created Content</Text>)}
+        <View style={styles.galleryView}>
+          <Text style={styles.latestPhotosText}>LATEST CONTENT</Text>
+          <View style={styles.photosFlatListViewWrapper}>
+            {this.props.artistTags[0] ? (
+              <FlatGrid
+                items={latestWorks}
+                renderItem={({ item }) => (
+                  <View style={styles.photosView}>
+                    <Image
+                      source={{ uri: item.arTagUrl }}
+                      style={styles.photosImage}
+                    />
+                  </View>
+                )}
+                itemDimension={180}
+                horizontal={true}
+                spacing={10}
+                style={styles.photosFlatList}
+              />
+            ) : (
+              <Text style={styles.drWhatText}>No User Created Content</Text>
+            )}
           </View>
         </View>
       </View>
@@ -142,7 +160,10 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  getTagsByArtist: userId => dispatch(getTagsByArtist(userId)),
+  getTagsByArtist: userId => dispatch(getTagsByArtist(userId))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Profile);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Profile);
